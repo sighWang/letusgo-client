9b9b2637e714b686002fe3ef356605f827f1a2e4
sigh <wangximingde@163.com>
2014-09-15 13:49:21 +0800

    change localStorage to use no model

--- app/scripts/model/localStorage.js
+++ app/scripts/model/localStorage.js
@@ -2,9 +2,9 @@
 
 function initData(localStorageService) {
   var goodsList = [];
-  var cocacola = new Goods('ITEM000000', '可口可乐', '瓶', 3.00, 'drink');
-  var sprite = new Goods('ITEM000001', '雪碧', '瓶', 3.00, 'drink');
-  var lychee = new Goods('ITEM000002', '荔枝', '斤', 15.00, 'fruit');
+  var cocacola ={id:'ITEM000000', name:'可口可乐', unit:'瓶', price:3.00, category:'drink'};
+  var sprite ={id:'ITEM000001', name:'雪碧', unit:'瓶', price:3.00, category:'drink'};
+  var lychee = {id:'ITEM000002', name:'荔枝', unit:'斤', price:15.00, category:'fruit'};
   var badminton = new Goods('ITEM000003', '羽毛球', '个', 4.50, 'sport');
   goodsList.push(cocacola, sprite, lychee, badminton);
   if (localStorageService.get('goodsList') === null) {
