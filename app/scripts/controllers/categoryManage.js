68a03c76d7b34cc44d7ca4cadfab9977217741c7
sigh <wangximingde@163.com>
2014-09-18 17:30:06 +0800

    format code

--- app/scripts/controllers/categoryManage.js
+++ app/scripts/controllers/categoryManage.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('letusgoApp')
-    .controller('CategoryManageCtrl', function ($scope, categoryService) {
+  .controller('CategoryManageCtrl', function ($scope, categoryService) {
     updateCategories();
 
 
     $scope.showEdit = function (category) {
-            categoryService.storeCategory(category);
-        };
+      categoryService.storeCategory(category);
+    };
 
-        $scope.removeItem = function (category) {
-            categoryService.removeCategory(category);
-          updateCategories();
-        };
-    function updateCategories () {
+    $scope.removeItem = function (category) {
+      categoryService.removeCategory(category);
+      updateCategories();
+    };
+    function updateCategories() {
       $scope.categorys = categoryService.getCategorys();
     }
-    });
+  });
