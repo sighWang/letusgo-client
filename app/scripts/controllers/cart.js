72c54959e69d12ec9aeafeebac461e601435b3ee
sigh <wangximingde@163.com>
2014-09-18 17:22:27 +0800

    format code

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -1,27 +1,28 @@
 'use strict';
 angular.module('letusgoApp')
-    .controller('CartCtrl', function ($scope, cartService) {
+  .controller('CartCtrl', function ($scope, cartService) {
 
-        $scope.$emit('cartHighLight');
+    $scope.$emit('cartHighLight');
     updatePage();
-        $scope.categoryNames = Object.keys(cartService.getCategory());
+    $scope.categoryNames = Object.keys(cartService.getCategory());
 
-        $scope.addOneToCart = function (item) {
-            cartService.addGoodsNumberById(item.goods.id);
-          updateCartNumber();
-          updatePage();
-        };
+    $scope.addOneToCart = function (item) {
+      cartService.addGoodsNumberById(item.goods.id);
+      updateCartNumber();
+      updatePage();
+    };
 
-        $scope.minusOneToCart = function (item) {
-            cartService.minusGoodsNumberById(item.goods.id);
-          updateCartNumber();
-          updatePage();
-        };
-        function updatePage(){
-          $scope.total = cartService.getTotal();
-          $scope.categorys = cartService.getCategory();
-        }
-    function updateCartNumber(){
+    $scope.minusOneToCart = function (item) {
+      cartService.minusGoodsNumberById(item.goods.id);
+      updateCartNumber();
+      updatePage();
+    };
+    function updatePage() {
+      $scope.total = cartService.getTotal();
+      $scope.categorys = cartService.getCategory();
+    }
+
+    function updateCartNumber() {
       $scope.$emit('updateCartNumber');
     }
-    });
+  });
