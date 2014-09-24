e22c7143c4d037c16c7f8488fcfac79e20e1248c
sigh <wangximingde@163.com>
2014-09-24 11:57:53 +0800

    delete useless test

--- test/spec/services/cartService.js
+++ test/spec/services/cartService.js
@@ -7,27 +7,9 @@
       module('letusgoApp');
       inject(function ($injector) {
         $scope = $injector.get('$rootScope').$new();
-        cartService = $injector.get('cartService');
+        cartService = $injector.get('CartService');
       });
 
-    });
-
-    it('getCustomGoodsList: result length should be a number', function () {
-      expect(cartService.getCustomGoodsList().length).toEqual(jasmine.any(Number));
-    });
-
-    it(' getGoodsList: result length should be 2', function () {
-      expect(cartService.getGoodslist().length).toBe(2);
-    });
-
-    it('editCustomGoodsList: should be called', function () {
-      cartService.addGoodsNumberById('ITEM000003');
-      var customGoodsList = cartService.getCustomGoodsList();
-      customGoodsList[0].number = 100;
-      spyOn(cartService, 'editCustomGoodsList');
-      cartService.editCustomGoodsList(customGoodsList);
-      expect(cartService.editCustomGoodsList).toHaveBeenCalledWith(customGoodsList);
-      expect(cartService.getCustomGoodsList()[0].number).toEqual(100);
     });
 
     it('addGoodNumberById: goodsNumber add one', function () {
@@ -45,10 +27,15 @@
       cartService.minusGoodsNumberById('ITEM000003');
       expect(customGoodsList[0]).toBeUndefined();
     });
+
     it('getTotal: should be a number', function () {
       expect(cartService.getTotal()).toEqual(jasmine.any(Number));
       expect(cartService.getTotal()).toEqual(9);
     });
+    it('categories should be got', function () {
+
+    });
+
     it('getCartNumber: should be a number', function () {
       expect(cartService.getCartNumber()).toEqual(2);
     });
