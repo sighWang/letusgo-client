46b120a272b32221aaa2b4dcc26a7f317ed71da7
sigh <wangximingde@163.com>
2014-09-25 20:52:02 +0800

    add check for addGoods.js

--- app/scripts/controllers/addGoods.js
+++ app/scripts/controllers/addGoods.js
@@ -1,10 +1,24 @@
 'use strict';
 angular.module('letusgoApp')
-  .controller('AddGoodsCtrl', function ($scope, GoodsListService, CategoryService) {
+  .controller('AddGoodsCtrl', function ($scope, GoodsListService, CategoryService, $location) {
     $scope.add = function (goods) {
-      GoodsListService.addGoods(goods);
-      $scope.$emit('refreshGoodsList');
-      $scope.$broadcast('refreshGoodsList');
+      if(goods === undefined){
+        $('#myModal').modal({});
+      }
+      else if(goods.id === undefined ||
+        goods.name === undefined ||
+        goods.unit === undefined ||
+        goods.price === undefined ||
+        goods.category === undefined){
+        $('#myModal').modal({});
+      }
+      else{
+        GoodsListService.addGoods(goods);
+        $scope.$emit('refreshGoodsList');
+        $scope.$broadcast('refreshGoodsList');
+        $location.path('/api/items/manage');
+      }
+
     };
 
     CategoryService.getCategories(function (data) {
