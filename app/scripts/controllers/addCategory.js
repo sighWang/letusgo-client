bcd3c1a87790c8096052b467486369658bd1c827
sigh <wangximingde@163.com>
2014-09-25 20:28:05 +0800

    add modal

--- app/scripts/controllers/addCategory.js
+++ app/scripts/controllers/addCategory.js
@@ -1,7 +1,19 @@
 'use strict';
 angular.module('letusgoApp')
-  .controller('AddCategoryCtrl', function ($scope, CategoryService) {
+  .controller('AddCategoryCtrl', function ($scope, CategoryService, $location) {
+    function showModal() {
+      $('#myModal').modal({});
+    }
     $scope.add = function (category) {
-      CategoryService.addCategory(category);
+      if(category === undefined){
+        showModal();
+      }
+      else if(category.id === undefined || category.name === undefined){
+        showModal();
+      }
+      else{
+        CategoryService.addCategory(category);
+        $location.path('/api/categories');
+      }
     };
   });
