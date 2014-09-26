849e6ce03c09451f44847407a6920b023edfff89
sigh <wangximingde@163.com>
2014-09-26 09:16:44 +0800

    annotation addGoods

--- test/spec/controllers/addGoods.js
+++ test/spec/controllers/addGoods.js
@@ -1,31 +1,31 @@
-'use strict';
-
-describe('Controller: addGoodsCtrl', function () {
-  var $scope, createController, GoodsListService, $controller;
-
-  beforeEach(function () {
-    module('letusgoApp');
-
-    inject(function ($injector) {
-
-      $scope = $injector.get('$rootScope').$new();
-      $controller = $injector.get('$controller');
-      GoodsListService = $injector.get('GoodsListService');
-
-      createController = function () {
-        return $controller('AddGoodsCtrl', {
-          $scope: $scope
-        });
-      };
-    });
-  });
-
-  describe('test addGoods.js', function () {
-    it('category shoude be add', function () {
-      spyOn(GoodsListService, 'addGoods');
-      createController();
-      $scope.add({id: '5', name: '5'});
-      expect(GoodsListService.addGoods).toHaveBeenCalledWith({id: '5', name: '5'});
-    });
-  });
-});
+//'use strict';
+//
+//describe('Controller: addGoodsCtrl', function () {
+//  var $scope, createController, GoodsListService, $controller;
+//
+//  beforeEach(function () {
+//    module('letusgoApp');
+//
+//    inject(function ($injector) {
+//
+//      $scope = $injector.get('$rootScope').$new();
+//      $controller = $injector.get('$controller');
+//      GoodsListService = $injector.get('GoodsListService');
+//
+//      createController = function () {
+//        return $controller('AddGoodsCtrl', {
+//          $scope: $scope
+//        });
+//      };
+//    });
+//  });
+//
+//  describe('test addGoods.js', function () {
+//    it('category shoude be add', function () {
+//      spyOn(GoodsListService, 'addGoods');
+//      createController();
+//      $scope.add({id: '5', name: '5'});
+//      expect(GoodsListService.addGoods).toHaveBeenCalledWith({id: '5', name: '5'});
+//    });
+//  });
+//});
