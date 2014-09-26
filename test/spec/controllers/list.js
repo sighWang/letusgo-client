669615313f5b965093de81224854a5f382674619
sigh <wangximingde@163.com>
2014-09-26 21:36:06 +0800

    update list.js

--- test/spec/controllers/list.js
+++ test/spec/controllers/list.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Controller: indexCtrl', function () {
-  var $scope, createController, cartService, $controller;
+  var $scope, createController, GoodsListService, $controller, cartService;
 
   beforeEach(function () {
     module('letusgoApp');
@@ -10,8 +10,8 @@ describe('Controller: indexCtrl', function () {
 
       $scope = $injector.get('$rootScope').$new();
       $controller = $injector.get('$controller');
+      GoodsListService = $injector.get('GoodsListService');
       cartService = $injector.get('CartService');
-
       createController = function () {
         return $controller('ListCtrl', {
           $scope: $scope
@@ -23,17 +23,25 @@ describe('Controller: indexCtrl', function () {
   describe('list.js', function () {
 
     it('$scope.goodsList: should be a number', function () {
+      var items = [{id:'ITEM000002', name:'荔枝', unit:'斤', price:15.00, category:'fruit'}];
+      spyOn(GoodsListService,'getGoodslist').and.callFake(function(callback){
+        callback(items);
+      });
       spyOn($scope, '$emit');
       createController();
+      GoodsListService.getGoodslist(function(data){
+        expect($scope.goodsList).toEqual(data);
+      });
       expect($scope.$emit).toHaveBeenCalledWith('listHighLight');
     });
 
-    it('addOneToCart:should call cartService.addGoodsNumberById', function () {
+    it('addOneToCart:should call cartService.updateGoodsNumberById', function () {
       createController();
       spyOn($scope, '$emit');
-      spyOn(cartService, 'addGoodsNumberById');
+      spyOn(cartService, 'updateGoodsNumberById');
       $scope.addOneToCart({id:'ITEM000002', name:'荔枝', unit:'斤', price:15.00, category:'fruit'});
       expect($scope.$emit).toHaveBeenCalledWith('updateCartNumber');
+      expect(cartService.updateGoodsNumberById).toHaveBeenCalledWith('ITEM000002', 1);
     });
   });
 });
