4e26f51ee232b0e7fcee5560e1d672c2118f7358
sigh <wangximingde@163.com>
2014-09-26 23:08:15 +0800

    regine pay.js

--- test/spec/controllers/pay.js
+++ test/spec/controllers/pay.js
@@ -1,44 +1,44 @@
-//'use strict';
-//
-//describe('Controller:PayCtrl', function () {
-//  var $scope, createController, cartService, $controller, localStorageService;
-//
-//  beforeEach(function () {
-//    module('letusgoApp');
-//
-//    inject(function ($injector) {
-//
-//      $scope = $injector.get('$rootScope').$new();
-//      $controller = $injector.get('$controller');
-//      cartService = $injector.get('cartService');
-//      localStorageService = $injector.get('localStorageService');
-//      createController = function () {
-//        return $controller('PayCtrl', {
-//          $scope: $scope
-//        });
-//      };
-//    });
-//    spyOn(localStorageService,'get').and.callFake(function () {
-//      return [{goods:{id: 'ITEM000003', name: '羽毛球', unit: '个', price: 4.50, category: 'sport'}, number:2}];
-//    });
-//  });
-//
-//  describe('pay.js', function () {
-//
-//    it('shoule be no highLight', function () {
-//      spyOn($scope, '$emit');
-//      createController();
-//      expect($scope.$emit).toHaveBeenCalledWith('payHighLight');
-//    });
-//
-//    it('$scope.goodsList: should be a number', function () {
-//      createController();
-//      expect($scope.customGoodsList.length).toEqual(jasmine.any(Number));
-//    });
-//
-//    it('$scope.goodsList: should be a number', function () {
-//      createController();
-//      expect($scope.total).toEqual(jasmine.any(Number));
-//    });
-//  });
-//});
+'use strict';
+
+describe('Controller:PayCtrl', function () {
+  var $scope, createController, cartService, $controller, localStorageService;
+
+  beforeEach(function () {
+    module('letusgoApp');
+
+    inject(function ($injector) {
+
+      $scope = $injector.get('$rootScope').$new();
+      $controller = $injector.get('$controller');
+      cartService = $injector.get('cartService');
+      localStorageService = $injector.get('localStorageService');
+      createController = function () {
+        return $controller('PayCtrl', {
+          $scope: $scope
+        });
+      };
+    });
+    spyOn(localStorageService,'get').and.callFake(function () {
+      return [{goods:{id: 'ITEM000003', name: '羽毛球', unit: '个', price: 4.50, category: 'sport'}, number:2}];
+    });
+  });
+
+  describe('pay.js', function () {
+
+    it('shoule be no highLight', function () {
+      spyOn($scope, '$emit');
+      createController();
+      expect($scope.$emit).toHaveBeenCalledWith('payHighLight');
+    });
+
+    it('$scope.goodsList: should be a number', function () {
+      createController();
+      expect($scope.customGoodsList.length).toEqual(jasmine.any(Number));
+    });
+
+    it('$scope.goodsList: should be a number', function () {
+      createController();
+      expect($scope.total).toEqual(jasmine.any(Number));
+    });
+  });
+});
