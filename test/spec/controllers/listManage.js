064a2135ead9ebea53b3d21d453cbdc35880fe67
sigh <wangximingde@163.com>
2014-09-26 22:28:40 +0800

    update listManage

--- test/spec/controllers/listManage.js
+++ test/spec/controllers/listManage.js
@@ -19,11 +19,13 @@ describe('Controller: ListmanageCtrl', function () {
     });
   });
 
-  describe('goodsListManageCtrl add remove edit get', function () {
-    it('should can get all goods', function () {
-      spyOn(goodsListService,'getGoodslist');
+  describe('ListmanageCtrl.js', function () {
+    it('test showEdit:GoodsListService.storeGoodsId should be called', function () {
+      var goods = {id:'ITEM000002', name:'荔枝', unit:'斤', price:15.00, category:'fruit'};
+      spyOn(goodsListService,'storeGoodsId');
       createController();
-      expect(goodsListService.getGoodslist).toHaveBeenCalled();
+      $scope.showEdit(goods);
+      expect(goodsListService.storeGoodsId).toHaveBeenCalledWith(goods.id);
     });
 
     it('removeItem : goodsList should be minus one', function () {
@@ -31,7 +33,7 @@ describe('Controller: ListmanageCtrl', function () {
       createController();
       var badminton = {id:'ITEM000002', name:'荔枝', unit:'斤', price:15.00, category:'fruit'};
       $scope.removeItem(badminton);
-      expect(goodsListService.removeGoods).toHaveBeenCalled();
+      expect(goodsListService.removeGoods).toHaveBeenCalledWith(badminton);
     });
   });
 });
