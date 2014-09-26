f14464f100b4eaf7a58f8c188b971e009f02986b
sigh <wangximingde@163.com>
2014-09-27 00:58:01 +0800

    update editGoods.js

--- test/spec/controllers/editGoods.js
+++ test/spec/controllers/editGoods.js
@@ -26,6 +26,21 @@ describe('Controller: EditGoodsCtrl', function () {
       createController();
       $scope.edit({id: '4', name: '5'});
       expect(GoodsListService.editGoods).toHaveBeenCalledWith({id: '4', name: '5'});
+    });
+
+    it('$scope.goods is get', function () {
+    var id = 'ITEM000000';
+    var items = {name: '可口可乐', unit: '瓶', price: 3.00, category: '饮料'};
+     spyOn(GoodsListService,'getGoods').and.callFake(function(id, callback){
+            callback(items);
+          });
+
+          createController();
+
+          GoodsListService.getGoods(id, function(data){
+            expect($scope.goods).toEqual(data);
+          });
+
     })
   });
 });
