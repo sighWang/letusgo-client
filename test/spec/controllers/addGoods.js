bace6c6376b21bf004c71b48980d8d1a94feb5b4
sigh <wangximingde@163.com>
2014-09-27 00:12:50 +0800

    update addGoods.js

--- test/spec/controllers/addGoods.js
+++ test/spec/controllers/addGoods.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Controller: addGoodsCtrl', function () {
-  var $scope, createController, GoodsListService, $controller;
+  var $scope, createController, GoodsListService, $controller, CategoryService;
 
   beforeEach(function () {
     module('letusgoApp');
@@ -11,7 +11,7 @@ describe('Controller: addGoodsCtrl', function () {
       $scope = $injector.get('$rootScope').$new();
       $controller = $injector.get('$controller');
       GoodsListService = $injector.get('GoodsListService');
-
+      CategoryService = $injector.get('CategoryService');
       createController = function () {
         return $controller('AddGoodsCtrl', {
           $scope: $scope
@@ -21,11 +21,22 @@ describe('Controller: addGoodsCtrl', function () {
   });
 
   describe('test addGoods.js', function () {
-    it('category shoude be add', function () {
+    it('category should be add', function () {
       spyOn(GoodsListService, 'addGoods');
       createController();
       $scope.add({id: '5', name: '5',unit: '5', price: '5', category: '5' });
       expect(GoodsListService.addGoods).toHaveBeenCalledWith({id: '5', name: '5',unit: '5', price: '5', category: '5' });
+    });
+
+    it('categories should be defined', function () {
+      var categories = ["饮料", "水果","运动器材"];
+      spyOn(CategoryService,'getCategories').and.callFake(function(callback){
+        callback(categories);
+      });
+      createController();
+      CategoryService.getCategories(function(data){
+        expect($scope.categories).toEqual(data);
+      });
     });
   });
 });
