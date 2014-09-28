74ce3c5774fa2cbd8c0eea5ee776fb0c5374ec2c
sigh <wangximingde@163.com>
2014-09-28 22:07:54 +0800

    update test

--- test/spec/services/cartService.js
+++ test/spec/services/cartService.js
@@ -19,37 +19,21 @@
     });
 
     it('test updateGoodsNumberById: goodsNumber add one', function () {
-//      cartService.addGoodsNumberById('ITEM000003');
-//      var customGoodsList = cartService.getCustomGoodsList();
-//      expect(customGoodsList[0].number).toBe(3);
-//      cartService.addGoodsNumberById('ITEM000002');
-//      customGoodsList = cartService.getCustomGoodsList();
-//      expect(customGoodsList[1].number).toBe(1);
-    });
-    it('test updateGoodsNumberById: goodsNumber minus one', function () {
-//      $httpBackend.expectPOST('/api/customItems/');
-
+      var id = 'IED00000';
+      $httpBackend.expectPOST('/api/customItems/' + id).respond('success');
+      spyOn($httpBackend, 'expectPOST');
+      cartService.updateGoodsNumberById(id, 'dsf');
+      $httpBackend.flush();
     });
 
     it('test getCategory: total should be a number', function () {
-//      var resData = 11;
-//      $httpBackend.expectGET('/api/customItems').respond(200, resData);
-//      cartService.getCategory(function (data){
-//        expect(data).toBe(11);
-//      });
-//      $httpBackend.flush();
-    });
-    it('test getCategory : should get custonItems from serve', function () {
-//      var result = 0;
-//      var callback = function (data) {
-//        result = data;
-//        expect(data).toBe(customItems[0].categories);
-//      };
-//      var customItems = [{categories:{goods:'goods', number: 'number', subtotal: 'subtatal'}, total: 'total'}];
-//      $httpBackend.expectGET('/api/customItems').respond(200, customItems);
-//      cartService.getCategory(callback);
-////      expect(result).toBe(customItems[0].categories);
-//      $httpBackend.flush();
+      var resData = {categories: 'category', total: 'total'};
+      $httpBackend.expectGET('/api/customItems').respond(200, resData);
+      cartService.getCategory(function (data, total){
+        expect(data).toBe('category');
+        expect(total).toBe('total');
+      });
+      $httpBackend.flush();
     });
 
     it('getCartNumber: should get cartNumber from serve', function () {
