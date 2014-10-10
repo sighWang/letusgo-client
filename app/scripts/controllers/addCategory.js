4027f739d76915a71d23821aabe895c07d8ac876
sigh <wangximingde@163.com>
2014-10-10 10:11:24 +0800

    change madol to tip

--- app/scripts/controllers/addCategory.js
+++ app/scripts/controllers/addCategory.js
@@ -3,12 +3,13 @@ angular.module('letusgoApp')
   .controller('AddCategoryCtrl', function ($scope, CategoryService, $location) {
     $scope.add = function (category) {
       if (!category) {
-        $('#myModal').modal({});
+        $scope.tip = '请完整填写信息';
       }
       else if (!category.id || !category.name) {
-        $('#myModal').modal({});
+        $scope.tip = '请完整填写信息';
       }
       else {
+        $scope.tip = '';
         CategoryService.addCategory(category);
         $location.path('/api/categories');
       }
